36112ea8ae0fed53950b0f9e47c85e2098e44dbb
Paul Schmidt <ps@fino.digital>
2021-05-06 13:22:09 +0200

    Move filters to computed properties
    filters are not supported in vue3 anymore, so migrate them to computed
    properties

--- simple-frontend-development-with-vuejs-nuxt-and-tailwindcss/code/hackernews-fino/components/Post.vue
+++ simple-frontend-development-with-vuejs-nuxt-and-tailwindcss/code/hackernews-fino/components/Post.vue
@@ -15,7 +15,7 @@
           <span class="underline">{{ post.user }}</span>
         </span>
 
-        <span> {{ post.time | timeAgo }} ago </span>
+        <span> {{ timeAgo }} ago </span>
         <span class="border-l pl-1"> {{ post.comments_count }} comments </span>
       </span>
     </div>
@@ -23,27 +23,52 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref, PropType } from '@vue/composition-api'
+
+type Post = {
+  url: string
+  time: string
+}
 
 export default defineComponent({
-    props: [
-        'post'
-    ],
-    computed: {
-      host(): string {
-        const host = post.url
-          .replace(/^https?:\/\//, '')
-          .replace(/\/.*$/, '')
-          .replace('?id=', '/')
-        const parts = host.split('.').slice(-3)
-        if (parts[0] === 'www') {
-          parts.shift()
-        }
-        return parts.join('.')
+  props: {
+    post: {
+      type: Object as PropType<Post>,
+      required: true,
+    },
+  },
+  methods: {
+    pluralize(time: number, label: string) {
+      if (time === 1) {
+        return time + label
+      }
+      return time + label + 's'
+    },
+  },
+  computed: {
+    host(): string {
+      const host = this.post.url
+        .replace(/^https?:\/\//, '')
+        .replace(/\/.*$/, '')
+        .replace('?id=', '/')
+      const parts = host.split('.').slice(-3)
+      if (parts[0] === 'www') {
+        parts.shift()
+      }
+      return parts.join('.')
+    },
+    timeAgo(): string {
+      const between = Date.now() / 1000 - Number(this.post.time)
+      if (between < 3600) {
+        return this.pluralize(~~(between / 60), ' minute')
+      } else if (between < 86400) {
+        return this.pluralize(~~(between / 3600), ' hour')
+      } else {
+        return this.pluralize(~~(between / 86400), ' day')
       }
     },
-    setup() {
-    },
+  },
+  setup() {},
 })
 </script>
 
